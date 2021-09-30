45b58afb92b4e8de7783461c65597ad2e0bd63cf
Rishu Yadav <72988817+rishuyadav@users.noreply.github.com>
2021-09-30 07:53:44 +0530

    GitPositive: Added Homescreen (#2)
    Signed-off-by: Rishu Yadav<rishuss1212@gmail.com>
    
    Co-authored-by: Gagan Malvi <malvigagan@gmail.com>

---
+++ app/src/main/java/org/acmvit/gitpositive/HomeScreen.kt
@@ -0,0 +1,34 @@
+package org.acmvit.gitpositive
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Html
+import android.view.LayoutInflater
+import org.acmvit.gitpositive.databinding.ActivityHomeScreenBinding
+import android.R.attr.action
+import android.app.Activity
+import android.content.Intent
+
+
+class HomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityHomeScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.Appname.setText(
+            Html.fromHtml(
+                getColorStr(
+                    "Git",
+                    "#6CFF54"
+                ) + getColorStr("Positive", "#FFFFFF")
+            )
+        )
+
+        val button = binding.floatingActionButton
+        button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
+fun getColorStr (text : String, color : String): String  =  "<font color=$color>$text</font>"
