f69d3ee7f3b8eb39ab4f8707dfcefc7a566833b4
Prasoon Soni <prasoonsoni03@gmail.com>
2021-10-02 12:42:21 +0530

    GitPositive: Added Dark and Light Mode, Enhanced UI, Added Loading Dialog, Added Validations for empty TextBox, Changed Fonts, Added Custom Cursor, (#8)

--- app/src/main/java/org/acmvit/gitpositive/HomeScreen.kt
+++ app/src/main/java/org/acmvit/gitpositive/HomeScreen.kt
@@ -2,8 +2,9 @@ package org.acmvit.gitpositive
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html
-import org.acmvit.gitpositive.databinding.ActivityHomeScreenBinding
 import android.content.Intent
+import android.widget.Toast
+import org.acmvit.gitpositive.databinding.ActivityHomeScreenBinding
 
 
 class HomeScreen : AppCompatActivity() {
@@ -12,13 +13,18 @@ class HomeScreen : AppCompatActivity() {
         val binding = ActivityHomeScreenBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        binding.Appname.text = Html.fromHtml(getColorStr("Git", "#6CFF54") + getColorStr("Positive", "#FFFFFF"))
+        binding.Appname.text = Html.fromHtml(getColorStr("Git", "#6CFF54") + getColorStr("Positive", getColor(R.color.text_color).toString()))
 
         val button = binding.floatingActionButton
         button.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra("Username",binding.username.text?.toString())
-            startActivity(intent)
+            if(binding.username.text.toString().isNotEmpty()){
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("Username",binding.username.text?.toString())
+                startActivity(intent)
+            } else {
+                Toast.makeText(applicationContext,"Username cannot be empty!!",Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
 }
