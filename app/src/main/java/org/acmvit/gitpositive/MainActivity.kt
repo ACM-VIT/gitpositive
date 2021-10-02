16f60629ea2380c0516921de9a0790c3cd347a43
Prasoon Soni <prasoonsoni03@gmail.com>
2021-10-02 11:28:57 +0530

    Added Dark and Light Mode, Enhanced UI, Added Loading Dialog, Added Validations for empty TextBox, Changed Fonts, Added Custom Cursor,

--- app/src/main/java/org/acmvit/gitpositive/MainActivity.kt
+++ app/src/main/java/org/acmvit/gitpositive/MainActivity.kt
@@ -1,8 +1,15 @@
 package org.acmvit.gitpositive
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.bumptech.glide.Glide
 import org.acmvit.gitpositive.databinding.ActivityMainBinding
 import retrofit2.Call
@@ -13,11 +20,13 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 var BaseURL="https://api.github.com/"
 class MainActivity : AppCompatActivity() {
+    private var loadingDialog: AlertDialog? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val Binding = ActivityMainBinding.inflate(layoutInflater)
         val view = Binding.root
         setContentView(view)
+        showLoadingDialog()
         getUserData(binding = Binding);
     }
 
@@ -28,7 +37,9 @@ class MainActivity : AppCompatActivity() {
                 .build()
                 .create(ApiInterface::class.java)
             val username= intent.getStringExtra("Username").toString()
-            binding.appName.text = Html.fromHtml(getColorStr("Git", "#6CFF54") + getColorStr("Positive", "#FFFFFF"))
+            binding.appName.text = Html.fromHtml(getColorStr("Git", "#6CFF54") + getColorStr("Positive",
+                getColor(R.color.text_color).toString()
+            ))
             val retrofitData = retrofitBuilder.getData(username)
             retrofitData.enqueue(object : Callback<UserData?> {
                 override fun onResponse(call: Call<UserData?>, response: Response<UserData?>) {
@@ -45,11 +56,25 @@ class MainActivity : AppCompatActivity() {
                         .override(200, 200)
                         .centerCrop()
                         .into(binding.avatar)
+                    hideLoadingDialog()
                 }
 
                 override fun onFailure(call: Call<UserData?>, t: Throwable) {
-
+                    Toast.makeText(applicationContext,t.message, Toast.LENGTH_SHORT).show()
+                    hideLoadingDialog()
                 }
             })
         }
+    fun showLoadingDialog(){
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.loading_dialog, null)
+        loadingDialog = AlertDialog.Builder(this)
+            .setView(dialogView)
+            .setCancelable(false)
+            .create()
+        loadingDialog!!.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        loadingDialog!!.show()
     }
+    fun hideLoadingDialog(){
+        loadingDialog!!.hide()
+    }
+}
