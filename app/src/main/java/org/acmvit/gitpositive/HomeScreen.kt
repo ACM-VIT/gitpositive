4a74c302918a55b44146088baa36e1662515e3e9
Prasoon Soni <prasoonsoni03@gmail.com>
2021-10-03 21:57:35 +0530

    Added Error Screen when no Internet Connection.

--- app/src/main/java/org/acmvit/gitpositive/HomeScreen.kt
+++ app/src/main/java/org/acmvit/gitpositive/HomeScreen.kt
@@ -6,7 +6,12 @@ import android.content.Intent
 import android.os.Build
 import android.os.VibrationEffect
 import android.os.Vibrator
+import android.view.View
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.lifecycle.Observer
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.textfield.TextInputLayout
 import dagger.hilt.android.AndroidEntryPoint
 import org.acmvit.gitpositive.databinding.ActivityHomeScreenBinding
 
@@ -14,11 +19,14 @@ import org.acmvit.gitpositive.databinding.ActivityHomeScreenBinding
 @AndroidEntryPoint
 class HomeScreen : AppCompatActivity() {
     var vibrator: Vibrator? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityHomeScreenBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+
         binding.Appname.text = Html.fromHtml(getColorStr("Git", "#6CFF54") + getColorStr("Positive", getColor(R.color.text_color).toString()))
 
         val button = binding.floatingActionButton
@@ -32,6 +40,7 @@ class HomeScreen : AppCompatActivity() {
                 Toast.makeText(applicationContext,"Username cannot be empty!!",Toast.LENGTH_SHORT).show()
             }
         }
+
     }
     fun doVibration() {
             vibrator = this.getSystemService(VIBRATOR_SERVICE) as Vibrator
@@ -41,6 +50,26 @@ class HomeScreen : AppCompatActivity() {
                     VibrationEffect.EFFECT_TICK
                 )
             )
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val noInternet = findViewById<ConstraintLayout>(R.id.noInternet)
+        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        val textBox = findViewById<TextInputLayout>(R.id.textInputLayout)
+        val networkConnection = NetworkConnection(applicationContext)
+        networkConnection.observe(this, Observer { isConnected->
+            if(isConnected){
+                noInternet.visibility = View.GONE
+                fab.visibility = View.VISIBLE
+                textBox.visibility = View.VISIBLE
+            } else {
+                Toast.makeText(applicationContext, "Internet is not connected", Toast.LENGTH_SHORT).show()
+                noInternet.visibility = View.VISIBLE
+                fab.visibility = View.GONE
+                textBox.visibility = View.GONE
+            }
+        })
     }
 }
 
