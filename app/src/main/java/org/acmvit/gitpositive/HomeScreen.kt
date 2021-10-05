f9474642f374b6491b7706dfcd8b07b105fc8493
rooparsh <rooparshkalia@gmail.com>
2021-10-05 17:37:54 +0530

    Fix : Issue 37

--- app/src/main/java/org/acmvit/gitpositive/HomeScreen.kt
+++ app/src/main/java/org/acmvit/gitpositive/HomeScreen.kt
@@ -1,17 +1,19 @@
 package org.acmvit.gitpositive
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.text.Html
+
 import android.content.Intent
-import android.os.Build
+import android.os.Bundle
 import android.os.VibrationEffect
 import android.os.Vibrator
 import android.view.View
+import android.text.Html
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.Observer
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.textfield.TextInputLayout
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import org.acmvit.gitpositive.databinding.ActivityHomeScreenBinding
 
@@ -20,36 +22,60 @@ import org.acmvit.gitpositive.databinding.ActivityHomeScreenBinding
 class HomeScreen : AppCompatActivity() {
     var vibrator: Vibrator? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val binding = ActivityHomeScreenBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+    private val viewModel: HomeViewModel by viewModels()
 
+    private var _binding: ActivityHomeScreenBinding? = null
 
-        binding.Appname.text = Html.fromHtml(getColorStr("Git", "#6CFF54") + getColorStr("Positive", getColor(R.color.text_color).toString()))
-
-        val button = binding.floatingActionButton
-        button.setOnClickListener {
-            doVibration()
-            if(binding.username.text.toString().isNotEmpty()){
-                val intent = Intent(this, MainActivity::class.java)
-                intent.putExtra("Username",binding.username.text?.toString())
-                startActivity(intent)
-            } else {
-                Toast.makeText(applicationContext,"Username cannot be empty!!",Toast.LENGTH_SHORT).show()
-            }
+    private val binding: ActivityHomeScreenBinding
+        get() {
+            return _binding!!
         }
 
-    }
-    fun doVibration() {
-            vibrator = this.getSystemService(VIBRATOR_SERVICE) as Vibrator
-            vibrator!!.vibrate(
-                VibrationEffect.createOneShot(
-                    50,
-                    VibrationEffect.EFFECT_TICK
-                )
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityHomeScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        observeViewModel()
+        binding.Appname.text = Html.fromHtml(
+            getColorStr("Git", "#6CFF54") + getColorStr(
+                "Positive",
+                getColor(R.color.text_color).toString()
             )
+        )
+
+        binding.floatingActionButton.setOnClickListener {
+            doVibration()
+            if (binding.username.text.toString().isNotEmpty()) {
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("Username", binding.username.text?.toString())
+                startActivity(intent)
+            } else {
+                Toast.makeText(applicationContext, "Username cannot be empty!!", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.userList.observe(this) {
+            it?.let { usersList ->
+                val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
+                    this,
+                    android.R.layout.simple_dropdown_item_1line, usersList
+                )
+                binding.username.setAdapter(adapter)
+            }
+        }
+    }
+
+    private fun doVibration() {
+        vibrator = this.getSystemService(VIBRATOR_SERVICE) as Vibrator
+        vibrator!!.vibrate(
+            VibrationEffect.createOneShot(
+                50,
+                VibrationEffect.EFFECT_TICK
+            )
+        )
     }
 
     override fun onResume() {
@@ -74,4 +100,4 @@ class HomeScreen : AppCompatActivity() {
 }
 
 
-fun getColorStr (text : String, color : String): String  =  "<font color=$color>$text</font>"
+fun getColorStr(text: String, color: String): String = "<font color=$color>$text</font>"
---
+++ app/src/main/java/org/acmvit/gitpositive/local/User.kt
@@ -0,0 +1,11 @@
+package org.acmvit.gitpositive.local
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class User(
+    @PrimaryKey val uid: Int,
+    @ColumnInfo(name = "username") val username: String?
+)
